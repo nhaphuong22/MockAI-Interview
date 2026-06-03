763917f433140033c5128a993eea77b04d468213
nhaphuong22 <nhaphuong220905@gmail.com>
2026-06-03 23:34:13 +0700

    update

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -7,9 +7,6 @@ import {
   updateJob,
   deleteJob
 } from '../controllers/jobController.js';
-import { authenticateToken, requireRole } from '../middleware/authMiddleware.js';
-
-import { createNewJob } from '../controllers/jobController.js';
 import { authenticateToken, requireRole } from '../middlewares/authMiddleware.js';
 
 
