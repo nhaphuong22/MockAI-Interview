bc1465d112269b4e984d69c785cdd2afdac9de5e
nhaphuong22 <nhaphuong220905@gmail.com>
2026-07-30 21:27:45 +0700

    Optimize Swagger initialization to save memory in Vercel Serverless environment

--- backend/src/config/swagger.js
+++ backend/src/config/swagger.js
@@ -37,10 +37,20 @@ const options = {
   ],
 };
 
-const swaggerSpec = swaggerJSDoc(options);
+let swaggerSpec = null;
 
 export const setupSwagger = (app) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log('📝 Swagger UI is enabled on http://localhost:5000/api-docs and http://localhost:5000/api-doc');
+  app.use('/api-docs', (req, res, next) => {
+    if (!swaggerSpec) {
+      swaggerSpec = swaggerJSDoc(options);
+    }
+    swaggerUi.setup(swaggerSpec)(req, res, next);
+  }, swaggerUi.serve);
+
+  app.use('/api-doc', (req, res, next) => {
+    if (!swaggerSpec) {
+      swaggerSpec = swaggerJSDoc(options);
+    }
+    swaggerUi.setup(swaggerSpec)(req, res, next);
+  }, swaggerUi.serve);
 };
